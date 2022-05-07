866a3267a4fc58f40201b468bafe7e49cc8f8ad2
ozpinar <ozpinarberk@gmail.com>
2022-05-08 02:59:13 +0300

    [feat] Add word selection

--- src/app/components/entry/entry.component.ts
+++ src/app/components/entry/entry.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntryComponent implements OnInit {
 
+  lorem = `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+  Quos cum ea aliquam blanditiis, saepe labore illo odit aliquid 
+  temporibus dicta vero harum deserunt tempora quas voluptatem 
+  ipsa corporis dolores nesciunt? Ad, ex deleniti corrupti quaerat 
+  explicabo suscipit excepturi, placeat perspiciatis consequuntur, 
+  saepe enim hic aliquam impedit ducimus officiis aliquid voluptatibus! 
+  Beatae eveniet temporibus neque asperiores consequuntur aspernatur 
+  maiores excepturi distinctio architecto nulla quo, expedita pariatur 
+  saepe sit est repellendus vero porro! Quas temporibus maiores alias?`
+
+  words = this.lorem.split(" ");
+
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void { }
 }
