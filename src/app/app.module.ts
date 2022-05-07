f88a7100dd691959b6e211de7b8df5db32a28c8d
ozpinar <ozpinarberk@gmail.com>
2022-05-07 04:22:07 +0300

    [fix] Post tailwind modification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopMenuComponent } from './components/top-menu/top-menu.component';
+import { EntryComponent } from './components/entry/entry.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopMenuComponent
+    TopMenuComponent,
+    EntryComponent
   ],
   imports: [
     BrowserModule,
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
     extend: {
       fontFamily: {
         'outfit': ['Outfit', 'sans-serif']
+      },
+      boxShadow: {
+        'post': '0px 0px 3px rgba(0, 0, 0, 0.25)'
       }
     },
     colors: {
